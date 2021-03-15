8772bac3a920ae12ea93e69a3325e61cb9a732bf
FrozenPriest <boral777@mail.ru>
2021-03-15 20:40:01 +0300

    SongDataAccess implemented

--- DataAccess/Contracts/ISongDataAccess.cs
+++ DataAccess/Contracts/ISongDataAccess.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 using Domain.Contracts;
@@ -9,9 +8,9 @@ namespace DataAccess.Contracts
 {
     public interface ISongDataAccess
     {
+        Task<IEnumerable<Song>> GetAsync();
         Task<Song> GetAsync(ISongIdentity songId);
-        Task<IEnumerable<Song>> GetAsync(IAlbumIdentity albumId);
-        
+
         Task<Song> InsertAsync(SongUpdateModel song);
         Task<Song> UpdateAsync(SongUpdateModel song);
     }
---
+++ DataAccess/Implementations/SongDataAccess.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataAccess.Context;
+using DataAccess.Contracts;
+using Domain;
+using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Implementations
+{
+    public class SongDataAccess : ISongDataAccess
+    {
+        private SongDirectoryContext Context { get; }
+        private IMapper Mapper { get; }
+
+
+        public SongDataAccess(SongDirectoryContext context, IMapper mapper)
+        {
+            Context = context;
+            Mapper = mapper;
+        }
+
+        public async Task<Song> InsertAsync(SongUpdateModel song)
+        {
+            var result = await Context.AddAsync(Mapper.Map<Song>(song));
+            await Context.SaveChangesAsync();
+
+            return Mapper.Map<Song>(result.Entity);
+        }
+
+        public async Task<IEnumerable<Song>> GetAsync()
+        {
+            return Mapper.Map<IEnumerable<Song>>(
+                await Context.Song.Include(x => x.Album).ToListAsync());
+        }
+
+        public async Task<Song> GetAsync(ISongIdentity songId)
+        {
+            var result = await Get(songId);
+            return Mapper.Map<Song>(result);
+        }
+
+        private async Task<object> Get(ISongIdentity song)
+        {
+            if (song == null)
+            {
+                throw new ArgumentNullException(nameof(song));
+            }
+
+            return await Context.Song.Include(x => x.Album)
+                .FirstOrDefaultAsync(x => x.Id == song.Id);
+        }
+
+
+        public async Task<Song> UpdateAsync(SongUpdateModel song)
+        {
+            var existing = await Get(song);
+            var result = Mapper.Map(song, existing);
+            Context.Update(result);
+            await Context.SaveChangesAsync();
+            return Mapper.Map<Song>(result);
+        }
+    }
+}
