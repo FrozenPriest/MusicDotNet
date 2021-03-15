bf99c7fad3cf0326f929fcdeef80b0b42177d1bb
FrozenPriest <boral777@mail.ru>
2021-03-15 20:00:35 +0300

    on model creating

--- DataAccess/Context/SongDirectoryContext.cs
+++ DataAccess/Context/SongDirectoryContext.cs
@@ -26,7 +26,32 @@ namespace DataAccess.Context
         {
             modelBuilder.Entity<Song>(entity =>
             {
-                //todo
+                entity.Property(e => e.Id).UseIdentityColumn();
+
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Duration).IsRequired();
+
+                entity.HasOne(song => song.Album)
+                    .WithMany(album => album.Song)
+                    .HasForeignKey(song => song.AlbumId)
+                    .HasConstraintName("FK_SONG_ALBUM");
+            });
+            
+            modelBuilder.Entity<Album>(entity =>
+            {
+                entity.Property(e => e.Id).UseIdentityColumn();
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.HasOne(album => album.Artist)
+                    .WithMany(artist => artist.Album)
+                    .HasForeignKey(album => album.ArtistId)
+                    .HasConstraintName("FK_ALBUM_ARTIST");
+            });
+            
+            modelBuilder.Entity<Artist>(entity =>
+            {
+                entity.Property(e => e.Id).UseIdentityColumn();
+                entity.Property(e => e.Name).IsRequired();
             });
         }
 
