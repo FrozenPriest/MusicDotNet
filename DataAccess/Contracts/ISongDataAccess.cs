09f25f5622fa1a28c31c292d492c87fbdc863e74
FrozenPriest <boral777@mail.ru>
2021-03-15 20:24:11 +0300

    refactoring, ISongDataAccess

---
+++ DataAccess/Contracts/ISongDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Domain.Contracts;
+using Domain.Models;
+
+namespace DataAccess.Contracts
+{
+    public interface ISongDataAccess
+    {
+        Task<Song> GetAsync(ISongIdentity songId);
+        Task<IEnumerable<Song>> GetAsync(IAlbumIdentity albumId);
+        
+        Task<Song> InsertAsync(SongUpdateModel song);
+        Task<Song> UpdateAsync(SongUpdateModel song);
+    }
+}
